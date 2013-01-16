3d5d4ac1683bdff399d175c5b0eb6a8399e31806
Aziz Khoury <bentael@gmail.com>
2013-01-15 21:55:49 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Candyhook::Application.config.secret_token = 'ef03f0a12a42a65ecb56267fa0e9e5da2219db2c46c04c3c07b725fbd65f708afe7ae828f1e915986c7161aba897137b659dcf88f05d94acc366eef219b819aa'
